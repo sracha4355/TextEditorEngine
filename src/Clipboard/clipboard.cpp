9d3f908d16fbcf15b7e1fd235d0446c4c67d6d3a
sracha4355 <sracharla014@gmail.com>
2023-01-10 21:15:11 -0500

    createdclipboard module, need to test it

--- src/Clipboard/clipboard.cpp
+++ src/Clipboard/clipboard.cpp
@@ -1,3 +1,23 @@
 #include "clipboard.h"
 
+Clipboard :: Clipboard (){
+	m_numOfLines = 0;
+}
+
+void Clipboard ::  insertLines(string * array, int numOfLines){
+	for(int i = 0; i < numOfLines; i++) content.push_back(array[i]);
+	m_numOfLines = numOfLines;
+}
+
+bool Clipboard ::  clearClipboard(){
+	if((int) content.size() == 0) {
+		cout << "Empty clipboard, nothing to clear" << endl;
+		return false;
+	}	
+	
+	content.clear();
+	return true;
+
+}
+
 
