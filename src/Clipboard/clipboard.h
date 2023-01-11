ba5eb6c4d4f915f1395de1dfd17591209ebd6ea2
sracha4355 <sracharla014@gmail.com>
2023-01-10 21:15:11 -0500

    createdclipboard module, need to test it

--- src/Clipboard/clipboard.h
+++ src/Clipboard/clipboard.h
@@ -1,2 +1,18 @@
 #pragma once
 #include "buffer.h"
+#include <vector>
+
+using namespace std;
+
+class Clipboard {
+	public:
+		Clipboard();
+		void insertLines(string*, int);
+		bool clearClipboard();
+		
+	private:
+
+		vector <string> content;
+		int m_numOfLines;
+		
+};
