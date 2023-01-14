3ec5cf122dc559ccb39d4659eeb937a62e01a2ab
sracha4355 <sracharla014@gmail.com>
2023-01-14 16:58:51 -0500

    implemented and tested undo and redo functionality

--- src/Window/window.cpp
+++ src/Window/window.cpp
@@ -1,16 +1,24 @@
+/*
+I am facing difficulties in finsihing the command module, so temporarily I will code the copy and paste commands here
+*/
+
 #include "window.h"
 
+// for existing files
 Window :: Window(string fileName){
+	m_buf = new TextBuffer;
+	m_cb = new Clipboard;
+	
 	File file;
 	file.m_fileName = fileName;
 	file.m_buffer = m_buf;
 	file.readData();
 }	
 
+// for new files
 Window :: Window(){
 	m_buf = new TextBuffer;
 	m_cb = new Clipboard;
-
 }
 
 Window :: ~Window(){
--- tests/Commands/commandTests.cpp
+++ tests/Commands/commandTests.cpp
@@ -1,6 +1,61 @@
 #include "commands.h"
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+void tCB(Window * win, string ch, int ct){
+	cout << "tCB ch: " << ch << " ct: " << ct << endl;
+}
+
+void tUB(Window * win, string ch, int ct){
+	cout << "tUB ch: " << ch << " ct: " << ct << endl;
+}
+
+
+TEST(undoRedo, insertingCallbacksIntoStack){
+	Window win;
+	commandsContainer cont(&win);
+	Command testCom;
+	testCom.cbFunctions = make_pair(tCB, tUB);
+	
+	
+	callbackFunc ptr1 = testCom.cbFunctions.first;
+	callbackFunc ptr2 = testCom.cbFunctions.second;
+
+	cont.addToUndo(testCom);
+	
+	cont.undo();
+	cont.redo();
+	
+	EXPECT_EQ(true, cont.isRedoEmpty());
+	EXPECT_EQ(false, cont.isUndoEmpty());
+	cout << cont.undoSize() << endl;
+	cout << cont.redoSize() << endl;	
+}	
+
+TEST(undoRedo, insertingMultipleTimesIntoUndoAndRedo){
+	Window win;
+	commandsContainer cont(&win);
+	Command testCom;
+	testCom.cbFunctions = make_pair(tCB,tUB);
+	
+	cont.addToUndo(testCom);
+	cont.addToUndo(testCom);
+	cont.addToUndo(testCom);
+	EXPECT_EQ(cont.undoSize(), 3);
+	
+	cont.undo();
+	EXPECT_EQ(cont.undoSize(), 2);
+	EXPECT_EQ(cont.redoSize(), 1);
+	cont.redo();
+	EXPECT_EQ(cont.undoSize(), 3);
+	EXPECT_EQ(cont.redoSize(), 0);
+	
+
+}
 
 int main(){
 	cout << "Command Tests" << endl;
-	return 0;
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS() ;
 }
